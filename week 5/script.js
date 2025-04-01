8ee068f57f845f0bbfc9f2fdbee08d48b5545b97
anika123rmit <s4095477@student.rmit.edu.au>
2025-04-01 12:16:41 +1100

    js

--- week 5/script.js
+++ week 5/script.js
@@ -9,7 +9,15 @@ myName = "abcd";
 console.log("Hi,I am", myName);
 let myNewName = prompt("What is your name");
 console.log("Hello, myNewName");
-let myNewName = "melbourne";
+let myNewName = "Melbourne";
 console.log("Hello", myNewName);
 let myCity = "Perth";
 console.log("Hello", myNewName, myCity);
+
+// if you know the value is not going to change, define it using const
+// or else use let
+{
+  let a = 10;
+  console.log("Hello", a);
+}
+console.log("Hello", a);
