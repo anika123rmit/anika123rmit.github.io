e93b5188dd1781161de7b90e86fee9fe7723306b
anika123rmit <s4095477@student.rmit.edu.au>
2025-04-01 11:29:01 +1100

    newww

---
+++ week 5/script.js
@@ -0,0 +1,15 @@
+console.log("Hi");
+console.log("how are you buddy");
+let myName = "anika";
+console.log("Hi,I am", myName);
+let myID = "s12345";
+console.log("Hi,my student id is", myID);
+// i am declaring a variable called myName
+myName = "abcd";
+console.log("Hi,I am", myName);
+let myNewName = prompt("What is your name");
+console.log("Hello, myNewName");
+let myNewName = "melbourne";
+console.log("Hello", myNewName);
+let myCity = "Perth";
+console.log("Hello", myNewName, myCity);
