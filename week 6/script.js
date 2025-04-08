3df32116d5235651933919b59e11bfe09a30045e
anika123rmit <s4095477@student.rmit.edu.au>
2025-04-08 12:16:11 +1000

    final week 6

--- week 6/script.js
+++ week 6/script.js
@@ -1,9 +1,18 @@
+const header = document.querySelector("header");
+console.log(header);
+console.log(header.innerHTML);
+header.innerHTML += "<h1> this is OART1013" </h1>";"
+console.log(header.innerHTML);
+
 const topHeading = document.querySelector("h1");
 console.log(topHeading.textContent);
-
 const myFirstPara = document.querySelector("p");
+topHeading.style.color="red";
 console.log(myFirstPara);
-for (let i=0; i< allParas[i]);
+for (let i=0; i< allParas[i];i++)
+{
+    
+}
 const sh1 = document.querySelector("first-subheading");
 console.log(sh1);
 
