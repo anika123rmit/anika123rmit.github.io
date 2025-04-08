2492957e90bbbd5426f9b08db561aaeac0aa30a6
anika123rmit <s4095477@student.rmit.edu.au>
2025-04-08 10:56:11 +1000

    week 6 part 2

---
+++ week 6/script.js
@@ -0,0 +1,14 @@
+const topHeading = document.querySelector("h1");
+console.log(topHeading.textContent);
+
+const myFirstPara = document.querySelector("p");
+console.log(myFirstPara);
+for (let i=0; i< allParas[i]);
+const sh1 = document.querySelector("first-subheading");
+console.log(sh1);
+
+const blue1 = document.querySelector(".blue-color");
+console.log(blue1);
+
+const allSubheadings = document.querySelectorAll("h2");
+console.log(allSubheadings);
