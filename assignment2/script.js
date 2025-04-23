bf9e3271f253c6a55770f5c10b0b3874fdf12d2f
anika123rmit <s4095477@student.rmit.edu.au>
2025-04-23 22:24:24 +1000

    newww

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,11 +1,13 @@
 // As the crafting video provided by canvas already had the play, pause, sound and full screen buttons,I had to introduce a new feature which was'nt already present.
-// The feature of buttons is a relevant feature in the context of my website since the media player is a crafting video tutorial.
-// Through the use of  buttons, users can jump to specific points in the video to see how the Windmill is being made.
+// The feature of buttons is a relevant item in the context of my website since the media player is a crafting video tutorial.
+// Through the use of  buttons, users can jump to specific points in the video to see how the Windmill is being made, and rewatch specific parts if they get stuck and need repetition.
 
 function seekVideo(seconds) {
   const video = document.getElementById("custom-video-player");
   if (video) {
-    video.currentTime = seconds;
+    video.currentTime = seconds;  
+    /* the currentTime function allows the user to play specific parts of the crafting tutorial.
     video.play();
   }
 }
+/* This code is linked to the <button onclick fucntion in index.html.
